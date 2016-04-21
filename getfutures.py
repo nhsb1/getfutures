252dd6839b6ba4ece90ae6c0a9396f11068c1f1e
nhsb1 <nhsb1@users.noreply.github.com>
2016-04-20 21:54:14 -0400

    functional

--- getfutures.py
+++ getfutures.py
@@ -45,10 +45,27 @@ print "Nikkei 225 Change: " + str(target3[310].text)
 #futures = scraper(url)
 #print futures
 
- i = 0
- for p in target3: #prints all the elements in the target3 list with line numbers to generate the map that I care about below.
- 	print i, p
- 	i += 1
+# i = 0
+# for p in target3: #prints all the elements in the target3 list with line numbers to generate the map that I care about below.
+# 	print i, p
+# 	i += 1
 
+#Map
+#print soup
+#print target3[3] #prints DJIA current price
+#print target3[4] #prints DJIA up/down
+#S&P 500 min price = 12
+#S&P 500 mini change = 13
+#NASDAQ price = 21
+#NASDAQ change = 22
+#Mexican IPC price = 30
+#Mexican IPC change = 31
+#VG1:IND - Euro STOXX 50 = 75
+#EUroStoxx50 change = 76
+#FTSE 100, Z 1:IND, price 84, change 85
+#GX1:IND - DAX30 = 93, change 94
+#XU1:IND - FTSE China A50, price = 264, change 265
+#HI1:IND - Hang Seng, price = 273, change = 274
+#NK1:IND - Nikkei 225, price = 309, change = 310
 
 
