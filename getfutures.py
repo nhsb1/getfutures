f03b565d055954e2e20c75734eb1f6839b50c215
nhsb1 <nhsb1@users.noreply.github.com>
2016-04-20 22:30:33 -0400

    minor changes

--- getfutures.py
+++ getfutures.py
@@ -7,33 +7,45 @@ import time
 #url = 'http://www.cnbc.com/pre-markets/'
 url = 'http://www.bloomberg.com/markets/stocks/futures'
 
-#def scraper(self):
 page = urllib2.urlopen(url)
-#soup = BeautifulSoup(page, 'html.parser')
 soup = BeautifulSoup(page, 'html5lib')
 target = soup.find("tr", {"class": "data-table__row"}) #gets DJIA instead of S&P 500
-#target2 = soup.find("tbody", {"class": "data-table__body"}) #gets the entire America's table
-#target2 = soup.find_all("tbody", {"class": "data-table__body"}) #finds a bunch of stuff
-target3 = soup.find_all("td")
+target = soup.find_all("td")
 
 print "Timestamp: %s" % (time.strftime('%Y-%m-%d %H:%M:%S'))
 print "Futures:"
-print "DJIA: " + str(target3[3].text)
-print "DJIA Change: " + str(target3[4].text)
-print "S&P 500: " + str(target3[12].text)
-print "S&P 500 Change: " + str(target3[13].text)
-print "NASDAQ: " + str(target3[21].text)
-print "NASDAQ: Change: "+ str(target3[22].text)
+dowprice = str(target[3].text)
+dowchange = str(target[4].text)
+spprice = str(target[12].text)
+spchange = str(target[13].text)
+nasdaqprice = str(target[21].text)
+nasdaqchange = str(target[22].text)
+europrice = str(target[75].text)
+eurochange = str(target[76].text)
+ftseprice = str(target[84].text)
+ftsechange = str(target[88].text)
+hangsengprice = str(target[264].text)
+hangsengchange = str(target[265].text)
+nikkeiprice = str(target[309].text)
+nikkeichange = str(target[310].text)
+
+
+print "DOW Price: " + dowprice
+print "DJIA Change: " + dowchange
+print "S&P 500: " + spprice
+print "S&P 500 Change: " + spchange
+print "NASDAQ: " + nasdaqprice
+print "NASDAQ: Change: "+ nasdaqchange
 print "---"
 print "ROW Futures"
-print "Euro STOXX 50: " + str(target3[75].text)
-print "Euro STOXX 50 Change: " + str(target3[76].text)
-print "FTSE 100: " + str(target3[84].text)
-print "FTSE 100 Change: " + str(target3[85].text)
-print "Hang Seng: " + str(target3[264].text)
-print "Hange Seng Change: " + str(target3[265].text)
-print "Nikkei 225: " + str(target3[309].text)
-print "Nikkei 225 Change: " + str(target3[310].text) 
+print "Euro STOXX 50: " + europrice
+print "Euro STOXX 50 Change: " + eurochange
+print "FTSE 100: " + ftseprice
+print "FTSE 100 Change: " + ftsechange
+print "Hang Seng: " + hangsengprice
+print "Hange Seng Change: " + hangsengchange
+print "Nikkei 225: " + nikkeiprice
+print "Nikkei 225 Change: " + nikkeichange 
 
 
 #target = soup.find("tbody", {"tr": "data-future-table-chart-symbol"}).span.contents
@@ -46,14 +58,14 @@ print "Nikkei 225 Change: " + str(target3[310].text)
 #print futures
 
 # i = 0
-# for p in target3: #prints all the elements in the target3 list with line numbers to generate the map that I care about below.
+# for p in target: #prints all the elements in the target list with line numbers to generate the map that I care about below.
 # 	print i, p
 # 	i += 1
 
 #Map
 #print soup
-#print target3[3] #prints DJIA current price
-#print target3[4] #prints DJIA up/down
+#print target[3] #prints DJIA current price
+#print target[4] #prints DJIA up/down
 #S&P 500 min price = 12
 #S&P 500 mini change = 13
 #NASDAQ price = 21
